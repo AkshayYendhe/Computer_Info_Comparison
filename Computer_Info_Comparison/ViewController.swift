ce5fa5c9a1f7ec043257a504afa90cb670875b0a
forloop <yendheakshayd@gmail.com>
2022-12-29 10:13:41 +0530

    Initial Commit

---
+++ Computer_Info_Comparison/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Computer_Info_Comparison
+//
+//  Created by Akshay Yendhe on 29/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
